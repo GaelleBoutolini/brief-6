d3d795badc39cc6fe609bc93990d652d7e8e10df
Hanaline <gaelle.boutolini.simplon@gmail.com>
2023-02-23 22:00:50 +0100

    Connexion-Form-Mobile_first

---
+++ Tools/formConnect.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="./style/style.css">
+    <title>Connexion</title>
+</head>
+<body>
+    <div class="container">
+          
+    <form method="post" action="">
+    <h2>Connexion</h2>
+        <div><label for="indentifiant">Indentifiant</label>
+        <input type="text" id="identifiant" name="identifiant" required></div>
+       
+    <div><label for="mot de passe">Mot de passe</label>
+<input type="text" id="mot de pass" name="mot de passe"required></div>
+
+        <input type="submit" value="Connexion">
+    </form>
+    </div>
+</body>
+</html>
