382ffc1fa23f6fc69b0799d79aff7b8157d21b6a
Hanaline <gaelle.boutolini.simplon@gmail.com>
2023-02-24 13:38:14 +0100

    ras

--- Tools/formInscrip.php
+++ Tools/formInscrip.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <div class="container">
-          
+           
     <form method="post" action="">
     <h2>Inscription</h2>
         <div><label for="nom">Nom:</label>
@@ -17,7 +17,6 @@
 
         <div><label for="prenom">Prénom</label>
         <input type="text" id="prenom" name="prenom" required></div>
-
         <div><label for="email">Adresse e-mail:</label>
         <input type="text" id="email" name="email" required></div>
         
@@ -33,20 +32,10 @@
         <div><label for="poids">Poids</label>
         <input type="number" id="poids" name="poids" required></div>
 
-        <div>
-        <label for="pet-select"></label>
-        <p>Activités</p>
-<select name="pets" id="pet-select">
-    <option value=""></option>
-    <option value="sédentaire : TMB x 1,2">Sédentaire</option>
-    <option value="légèrement actif : TMB x 1,375">Légèrement actif</option>
-    <option value="modérément actif : TMB x 1,55">Modérément actif</option>
-    <option value=" très actif : TMB x 1,725"> Très actif</option>
-    <option value=" extrêmement actif : TMB x 1,9"> Extrêmement actif</option>
-</select></div>
-       
-    <div><label for="mot de passe">Mot de passe</label>
+        <div><label for="mot de passe">Mot de passe</label>
 <input type="text" id="mot de pass" name="mot de passe"required></div>
+       
+        
 
         <input type="submit" value="Inscription">
     </form>
