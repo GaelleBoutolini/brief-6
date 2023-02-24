4d6a8a9223b5e578e907452bf3ce909a932b8f0f
Haroun <hrnabd@hotmail.com>
2023-02-24 16:19:30 +0100

    homepageadded but not finished

--- Vue/Home.php
+++ Vue/Home.php
@@ -1,38 +1,29 @@
-
 <?php ob_start(); ?>
-<h1>Home</h1>
 
+
+
+
+
+<div class="msgDeBienvenue"> Bienvenue sur <div>Equilibra</div>
+</div>
+<div class=" firstMsgDaccueilUser">
+    <img src="./Tools/image/illustration-accueil.webp" class="imgAccueil">
+    <p class="msgDaccueilUser"> Être en forme n'as jamais été aussi facile!! </p>
+</div>
+<div class="secndMsgDaccueilUser">
+    <form>
+        <p class="msgDaccueilUser2"> Vous avez déjà utilisé notre app</P>
+        <input type="submit" name="log-in" value="Se connecter">
+
+        <p class="msgDaccueilUser2"> Vous nêtes pas encore inscrit </P>
+        <input type="submit" name="sign-up" value="Inscrivez-vous">
+    </form>
+</div>
+
+
+
+
+<?php $title = 'Home' ?>
+<?php $style = './Tools/style/Home.css' ?>
 <?php $contenu = ob_get_clean(); ?>
 <?php require 'Template.php'; ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../tools/style/Home.css">
-    <title>pagAccueil</title>
-</head>
-    <body>
-        <div class = " msgDeBienvenue"> Bienvenue sur <div>Equilibra</div></div>
-        <div class = " firstMsgDaccueilUser">
-            <img src="../tools/image/illustration-accueil.webp" class="imgAccueil">
-            <p class="msgDaccueilUser" > Être en forme n'as jamais  été aussi facile!! </p>
-        </div>
-        <div  class = "secndMsgDaccueilUser"> 
-            <form>
-                <p class="msgDaccueilUser2"> Vous avez déjà utilisé notre app</P>
-                <input type="submit" name ="log-in" value="Se connecter">
-
-                <p class="msgDaccueilUser2"> Vous nêtes pas encore inscrit  </P>
-                <input type="submit"  name ="sign-up" value="Inscrivez-vous">
-            </form>
-
-        </div>
-
-
-
-        
-    </body>
-</html>
--- Vue/Template.php
+++ Vue/Template.php
@@ -1,8 +1,3 @@
-<?php
-$style = "./Tools/style/Template.css";
-$title = "";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,6 +5,7 @@ $title = "";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./Tools/style/Template.css" rel="stylesheet">
     <link href="<?= $style ?>" rel="stylesheet">
     <title><?= $title ?></title>
 </head>
