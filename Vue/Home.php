043d1ffda4be8272f39591d14d8bc8cf89610ddb
timsydrame <fatimdrame111@gmail.com>
2023-03-06 09:18:32 +0100

    la page d'accueil

--- Vue/Home.php
+++ Vue/Home.php
@@ -1,20 +1,22 @@
-<?php ob_start(); ?>
-
-<h2 class="msgDeBienvenue"> Bienvenue sur Equilibra</h2>
-<div class=" firstMsgDaccueilUser">
-    <img src="./Tools/image/illustration-accueil.webp" class="imgAccueil">
-    <p class="msgDaccueilUser"> Être en forme n'as jamais été aussi facile!! </p>
+<<?php ob_start(); ?>
+<main>
+<h1 class= "msg"> Bienvenue sur Equilibra</h1>
+        
+        
+<div class = "firstMsgDaccueilUser">
+    <img src="./Tools/image/illustration-accueil.webp" >
+    <h2 > Être en forme n'as jamais  été aussi facile!! </h2>
 </div>
-<div class="secondMsgDaccueilUser">
+
+<div class = "secndMsgDaccueilUser"> 
     <form>
-        <p class="msgDaccueilUser2"> Vous avez déjà utilisé notre app</P>
-        <input type="submit" name="log-in" value="Se connecter">
-
-        <p class="msgDaccueilUser2"> Vous n'êtes pas encore inscrit </P>
-        <input type="submit" name="sign-up" value="Inscrivez-vous">
+        <p class="msgDaccueilUser2"> Vous avez déjà utilisé notre app</p>
+        <input type="submit" name ="log-in" value="Se connecter">
+        <p class="msgDaccueilUser2"> Vous n'êtes pas encore inscrit  </p>
+        <input type="submit"  name ="sign-up" value="Inscrivez-vous">
     </form>
-</div>
-
+</div>  
+</main>
 <?php $logout = '' ?>
 <?php $title = 'Home' ?>
 <?php $style = './Tools/style/Home.css' ?>
